39ce4f1a11d61d1ef1920d32169775781c409ab7
sunmoon2222@gmail.com <sunmoon2222@gmail.com@ae2ab87b-24aa-aa42-1568-339c616a11e5>
2013-05-12 10:07:59 +0000

    git-svn-id: http://ddpy.googlecode.com/svn/trunk@170 ae2ab87b-24aa-aa42-1568-339c616a11e5

---
+++ Tools/FrmTools.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Text
+
+Public Class FrmTools
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        'Dim path As String = "C:/Users/zhangm/Desktop/GJDQ.txt"
+
+        'Dim txt As String = My.Computer.FileSystem.ReadAllText(path, Encoding.UTF8)
+        'Dim lines As String() = txt.Split(vbNewLine)
+        'Dim cols As String()
+        'Dim buf As New StringBuilder
+
+        'For i As Integer = 0 To lines.Length - 1
+        '    Dim line As String = Trim(lines(i).Replace(vbLf, ""))
+        '    If "".Equals(line) OrElse IsNumeric(line) Then
+        '        Continue For
+        '    End If
+
+        '    buf.AppendLine(line)
+        'Next
+
+        'My.Computer.FileSystem.WriteAllText(path, buf.ToString, False, Encoding.UTF8)
+        'Me.Close()
+
+    End Sub
+End Class
